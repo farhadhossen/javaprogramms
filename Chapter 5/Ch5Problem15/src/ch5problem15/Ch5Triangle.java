c43bbaf92e07f3d258953ac3157bf1ce369de416
farhadhossen <imfarhad01@gmail.com>
2022-03-20 18:08:51 +0600

    ch5 problem 15 running

---
+++ Chapter 5/Ch5Problem15/src/ch5problem15/Ch5Triangle.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch5problem15;
+
+/**
+ *
+ * @author Farhad
+ */
+public class Ch5Triangle {
+    
+    public static final int INVALID_DIMENSION = -1;
+    private boolean isValid;
+    public static double a, b, c, temp; 
+    
+    public Ch5Triangle(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        checkValid();
+    }
+    
+    public void checkValid(){
+        if(a>0){
+            
+        }
+    }
+        
+    
+}
