f0b2dbde124c8adabecb92f49b1ae947327fcbcb
farhadhossen <imfarhad01@gmail.com>
2022-03-24 12:50:29 +0600

    chapter 6 problem 23

---
+++ Chapter 4/Ch6Problem23/src/ch6problem23/Ch6Problem23.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ch6problem23;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author FARHAD
+ */
+public class Ch6Problem23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int x=0, intLen;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        x = sc.nextInt();
+        
+        intLen = String.valueOf(x).length();
+        
+
+        
+        for(int i=intLen; i<(5*3+intLen); i+=3){
+            System.out.format("%" + (i) + "d\n", x);
+        }
+
+
+
+    }
+    
+}
