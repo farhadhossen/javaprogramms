2712501e120e2a0a54a8ca007c60d9aa0b09055f
farhadhossen <imfarhad01@gmail.com>
2022-03-31 17:17:36 +0600

    ch7 problem 16 done

---
+++ Chapter 8/Ch8Problem6/src/ch8problem6/Ch8Problem6.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ch8problem6;
+
+/**
+ *
+ * @author Farhad
+ */
+public class Ch8Problem6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+        AgeInputVer4 ageInputVer4 = new AgeInputVer4(1,130);
+        
+        ageInputVer4.getAge();
+    }
+    
+}
